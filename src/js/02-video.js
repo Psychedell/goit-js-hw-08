cee45390a238197245a9e095ebeaa5d67c3a3736
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-08-08 14:22:29 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlayCurrentTime = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+  //   console.log(seconds);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(onPlayCurrentTime, 1000));
